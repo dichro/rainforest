98b5ee742dab78e3d80388c06a88221c2b1edcba
Miki Habryn <miki@rcpt.to>
2018-12-27 15:27:19 -0800

    parse hexstrings in XML unmarshal

--- emu-2.go
+++ emu-2.go
@@ -2,23 +2,50 @@ package main
 
 import (
 	"encoding/xml"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 
 	"github.com/golang/glog"
 )
 
+// HexInt64 is an int64 that hex-decodes itself on UnmarshalXML.
+type HexInt64 int64
+
+func (n *HexInt64) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	token, err := d.Token()
+	if err != nil {
+		return err
+	}
+	cd, ok := token.(xml.CharData)
+	if !ok {
+		return errors.New("expected CDATA")
+	}
+	// we don't own cd, so let's parse it before we call Skip or anything else in Decoder
+	num, err := strconv.ParseInt(string(cd), 0, 64)
+	if err != nil {
+		return err
+	}
+	// are we guaranteed to have consumed all the CDATA now?
+	if err := d.Skip(); err != nil {
+		return err
+	}
+	*n = HexInt64(num)
+	return err
+}
+
 type InstantaneousDemand struct {
 	DeviceMacId         string
 	MeterMacId          string
-	TimeStamp           string
-	Demand              string
-	Multiplier          string
-	Divisor             string
-	DigitsRight         string
-	DigitsLeft          string
+	TimeStamp           HexInt64
+	Demand              HexInt64
+	Multiplier          HexInt64
+	Divisor             HexInt64
+	DigitsRight         HexInt64
+	DigitsLeft          HexInt64
 	SuppressLeadingZero string
 }
 
--- emu-2_test.go
+++ emu-2_test.go
@@ -1,4 +1,11 @@
-<InstantaneousDemand>
+package main
+
+import (
+	"encoding/xml"
+	"testing"
+)
+
+const emu2message = `<InstantaneousDemand>
   <DeviceMacId>0xd8d5b9000000f919</DeviceMacId>
   <MeterMacId>0x00135003004653a5</MeterMacId>
   <TimeStamp>0x23b810ea</TimeStamp>
@@ -8,4 +15,11 @@
   <DigitsRight>0x03</DigitsRight>
   <DigitsLeft>0x0f</DigitsLeft>
   <SuppressLeadingZero>Y</SuppressLeadingZero>
-</InstantaneousDemand>
+</InstantaneousDemand>`
+
+func TestEMU2Message(t *testing.T) {
+	var m EMU2Message
+	if err := xml.Unmarshal([]byte(emu2message), &m); err != nil {
+		t.Fatalf("unmarshal failed: %s", err)
+	}
+}
